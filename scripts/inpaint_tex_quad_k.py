ea5769c3858fb89e70bd2700e548b0ab0fcf4d3f
Yifan Wang <wangyifan1995@gmail.com>
2022-10-17 21:09:19 -0700

    add rand crop

--- scripts/inpaint_tex_quad_k.py
+++ scripts/inpaint_tex_quad_k.py
@@ -5,8 +5,23 @@ from tqdm import tqdm
 import numpy as np
 import torch
 from main import instantiate_from_config
+import k_diffusion as K
+import accelerate
 from ldm.models.diffusion.ddim import DDIMSampler
 from ldm.models.diffusion.plms import PLMSSampler
+
+
+class CFGDenoiser(torch.nn.Module):
+    def __init__(self, model):
+        super().__init__()
+        self.inner_model = model
+
+    def forward(self, x, sigma, uncond, cond, cond_scale):
+        x_in = torch.cat([x] * 2)
+        sigma_in = torch.cat([sigma] * 2)
+        cond_in = torch.cat([uncond, cond])
+        uncond, cond = self.inner_model(x_in, sigma_in, cond=cond_in).chunk(2)
+        return uncond + (cond - uncond) * cond_scale
 
 
 def make_batch(image, img_path, device):
@@ -98,18 +113,18 @@ if __name__ == "__main__":
     )
     opt = parser.parse_args()
 
-    opt.outdir = '/home/yifan1/Desktop/latent-diffusion/outputs/inpainting_results_quad_rot_2.0'
+    opt.outdir = '/home/yifan1/Desktop/latent-diffusion/outputs/rock_7_quad_rot_lms'
     # config_path = '/home/yifan1/Desktop/latent-diffusion/models/ldm/inpainting_tex/tex-ldm-vq-f4-ipt.yaml'
     # ckpt_path = '/home/yifan1/Desktop/latent-diffusion/logs/2022-10-03T17-12-41_tex-ldm-vq-f4-ipt/checkpoints/last.ckpt'
 
     # config_path = '/home/yifan1/Desktop/latent-diffusion/models/ldm/inpainting_tex/tex-ldm-vq-f4-ipt.yaml'
     # ckpt_path = '/home/yifan1/Desktop/latent-diffusion/logs/2022-10-05T21-16-45_tex-ldm-vq-f4-ipt/checkpoints/last.ckpt'
 
-    config_path = '/home/yifan1/Desktop/latent-diffusion/models/ldm/inpainting_tex/tex-ldm-kl-f8-ipt.yaml'
+    config_path = '/home/yifan1/Desktop/latent-diffusion/models/ldm/inpainting_tex/tex-ldm-vq-f8-ipt.yaml'
     ckpt_path = '/home/yifan1/Desktop/latent-diffusion/logs/2022-10-07T20-27-00_tex-ldm-vq-f8-ipt/checkpoints/last.ckpt'
 
-    # config_path = '/home/yifan1/Desktop/latent-diffusion/models/ldm/inpainting_tex/tex-ldm-kl-4-ipt.yaml'
-    # ckpt_path = '/home/yifan1/Desktop/latent-diffusion/logs/2022-09-30T12-29-58_tex-ldm-kl-4-ipt/checkpoints/last.ckpt'
+    # config_path = '/home/yifan1/Desktop/latent-diffusion/models/ldm/inpainting_tex/tex-ldm-vq-f4-n256-ipt.yaml'
+    # ckpt_path = '/home/yifan1/Desktop/latent-diffusion/logs/2022-10-11T18-01-48_tex-ldm-vq-f4-n256-ipt/checkpoints/last.ckpt'
 
     config = OmegaConf.load(config_path)
     model = instantiate_from_config(config.model)
@@ -118,11 +133,18 @@ if __name__ == "__main__":
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     model = model.to(device)
     # sampler = DDIMSampler(model)
-    sampler = PLMSSampler(model)
+    # sampler = PLMSSampler(model)
+
+    model_wrap = K.external.CompVisDenoiser(model)
+    sigma_min, sigma_max = model_wrap.sigmas[0].item(), model_wrap.sigmas[-1].item()
 
     n_img = 7
     n_saved = 0
-    uc_scale = 2.0
+    uc_scale = 2.25
+    opt.outdir = f'{opt.outdir}_{uc_scale}'
+    print(opt.outdir)
+    accelerator = accelerate.Accelerator()
+
     img_prev = None
     os.makedirs(opt.outdir, exist_ok=True)
     img_out = np.zeros((256 + 128 * (n_img - 1), 256 + 128 * (n_img - 1), 3), dtype=np.uint8)
@@ -141,25 +163,26 @@ if __name__ == "__main__":
                                                      size=c.shape[-2:])
                 c = torch.cat((c, cc[:, :1, ...]), dim=1)
 
-                if i < 0:
-                    shape = (c.shape[1] - 1,) + c.shape[2:]
-                    samples_ddim, _ = sampler.sample(S=opt.steps,
-                                                     conditioning=c,
-                                                     batch_size=c.shape[0],
-                                                     shape=shape,
-                                                     verbose=False)
-                else:
-                    uc = model.get_learned_conditioning(torch.zeros_like(batch["image"]).fill_(-1.))
-                    uc = torch.cat((uc, torch.ones_like(cc[:, :1, ...])), dim=1)
+                uc = model.get_learned_conditioning(torch.zeros_like(batch["image"]).fill_(-1.))
+                uc = torch.cat((uc, torch.ones_like(cc[:, :1, ...])), dim=1)
 
-                    shape = (c.shape[1] - 1,) + c.shape[2:]
-                    samples_ddim, _ = sampler.sample(S=opt.steps,
-                                                     conditioning=c,
-                                                     batch_size=c.shape[0],
-                                                     shape=shape,
-                                                     unconditional_conditioning=uc,
-                                                     unconditional_guidance_scale=uc_scale,
-                                                     verbose=False)
+                shape = (c.shape[1] - 1,) + c.shape[2:]
+
+                # x0 = model.get_first_stage_encoding(model.encode_first_stage(batch["masked_image"]))  # move to latent space
+                # x0 = torch.randn(shape, device=device).unsqueeze(0)
+                # sigmas = model_wrap.get_sigmas(opt.steps)
+                # torch.manual_seed(23)  # changes manual seeding procedure
+                # noise = torch.randn_like(x0) * sigmas[opt.steps - t_enc - 1]  # for GPU draw
+                # xi = x0 + noise
+                # sigma_sched = sigmas[opt.steps - t_enc - 1:]
+
+                sigmas = model_wrap.get_sigmas(opt.steps)
+                xi = torch.randn(shape, device=device).unsqueeze(0) * sigmas[0]
+
+                model_wrap_cfg = CFGDenoiser(model_wrap)
+                extra_args = {'cond': c, 'uncond': uc, 'cond_scale': uc_scale}
+                samples_ddim = K.sampling.sample_lms(model_wrap_cfg, xi, sigmas, extra_args=extra_args,
+                                                     disable=not accelerator.is_main_process)
                 x_samples_ddim = model.decode_first_stage(samples_ddim)
                 img_prev = x_samples_ddim.detach().clone()
 
@@ -193,13 +216,14 @@ if __name__ == "__main__":
                 uc = torch.cat((uc, torch.ones_like(cc[:, :1, ...])), dim=1)
 
                 shape = (c.shape[1] - 1,) + c.shape[2:]
-                samples_ddim, _ = sampler.sample(S=opt.steps,
-                                                 conditioning=c,
-                                                 batch_size=c.shape[0],
-                                                 shape=shape,
-                                                 unconditional_conditioning=uc,
-                                                 unconditional_guidance_scale=uc_scale,
-                                                 verbose=False)
+
+                sigmas = model_wrap.get_sigmas(opt.steps)
+                xi = torch.randn(shape, device=device).unsqueeze(0) * sigmas[0]
+
+                model_wrap_cfg = CFGDenoiser(model_wrap)
+                extra_args = {'cond': c, 'uncond': uc, 'cond_scale': uc_scale}
+                samples_ddim = K.sampling.sample_lms(model_wrap_cfg, xi, sigmas, extra_args=extra_args,
+                                                     disable=not accelerator.is_main_process)
                 x_samples_ddim = model.decode_first_stage(samples_ddim)
                 img_prev = x_samples_ddim.detach().clone()
 
@@ -234,13 +258,13 @@ if __name__ == "__main__":
                     uc = torch.cat((uc, torch.ones_like(cc[:, :1, ...])), dim=1)
 
                     shape = (c.shape[1] - 1,) + c.shape[2:]
-                    samples_ddim, _ = sampler.sample(S=opt.steps,
-                                                     conditioning=c,
-                                                     batch_size=c.shape[0],
-                                                     shape=shape,
-                                                     unconditional_conditioning=uc,
-                                                     unconditional_guidance_scale=uc_scale,
-                                                     verbose=False)
+                    sigmas = model_wrap.get_sigmas(opt.steps)
+                    xi = torch.randn(shape, device=device).unsqueeze(0) * sigmas[0]
+
+                    model_wrap_cfg = CFGDenoiser(model_wrap)
+                    extra_args = {'cond': c, 'uncond': uc, 'cond_scale': uc_scale}
+                    samples_ddim = K.sampling.sample_lms(model_wrap_cfg, xi, sigmas, extra_args=extra_args,
+                                                         disable=not accelerator.is_main_process)
                     x_samples_ddim = model.decode_first_stage(samples_ddim)
                     img_prev = x_samples_ddim.detach().clone()
 
@@ -257,49 +281,3 @@ if __name__ == "__main__":
                     img_out[(i + 2) * 128:(i + 1) * 128 + 256, (j + 2) * 128:(j + 1) * 128 + 256, :] = \
                         inpainted[128:, 128:, :]
             Image.fromarray(img_out).save(os.path.join(opt.outdir, 'tile.png'))
-            # exit()
-            #
-            # for j in tqdm(range(n_img)):
-            #     for i in range(n_img):
-            #         outpath = os.path.join(opt.outdir, '{:02d}.png'.format(j * n_img + i))
-            #         if j == 0:
-            #             batch = make_batch(img_prev, opt.img_path, device=device)
-            #         else:
-            #             img_up = img_out[j*128:j*128+256, i*128:i*128+256, :].copy()
-            #             batch = make_batch_v(img_up, device=device)
-            #
-            #         # encode masked image and concat downsampled mask
-            #         c = model.cond_stage_model.encode(batch["masked_image"])
-            #         cc = torch.nn.functional.interpolate(batch["mask"],
-            #                                              size=c.shape[-2:])
-            #         c = torch.cat((c, cc[:, :1, ...]), dim=1)
-            #
-            #         # uc = model.get_learned_conditioning(batch["masked_image"])
-            #         # uc = torch.cat((uc, cc[:, :1, ...]), dim=1)
-            #
-            #         uc = model.get_learned_conditioning(torch.zeros_like(batch["image"]))
-            #         uc = torch.cat((uc, torch.ones_like(cc[:, :1, ...])), dim=1)
-            #
-            #         shape = (c.shape[1]-1,)+c.shape[2:]
-            #         samples_ddim, _ = sampler.sample(S=opt.steps,
-            #                                          conditioning=c,
-            #                                          batch_size=c.shape[0],
-            #                                          shape=shape,
-            #                                          unconditional_conditioning=uc,
-            #                                          unconditional_guidance_scale=1.1,
-            #                                          verbose=False)
-            #         x_samples_ddim = model.decode_first_stage(samples_ddim)
-            #         img_prev = x_samples_ddim.detach().clone()
-            #
-            #         image = torch.clamp((batch["image"]+1.0)/2.0,
-            #                             min=0.0, max=1.0)
-            #         mask = torch.clamp((batch["mask"]+1.0)/2.0,
-            #                            min=0.0, max=1.0)
-            #         predicted_image = torch.clamp((x_samples_ddim+1.0)/2.0,
-            #                                       min=0.0, max=1.0)
-            #
-            #         inpainted = (1-mask)*image+mask*predicted_image
-            #         inpainted = inpainted.cpu().numpy().transpose(0,2,3,1)[0]*255
-            #         Image.fromarray(inpainted.astype(np.uint8)).save(outpath)
-            #         img_out[j*128:j*128+256, i*128:i*128+256, :] = inpainted
-            # Image.fromarray(img_out).save(os.path.join(opt.outdir, 'tile.png'))
